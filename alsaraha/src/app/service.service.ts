dc679c0987aa1a8c7eacf8169b8790a46417eee8
a0952432706 <abdulfattah.khudari@gmail.com>
2018-04-14 12:47:50 +0300

    'a'

--- alsaraha/src/app/service.service.ts
+++ alsaraha/src/app/service.service.ts
@@ -6,7 +6,7 @@ import {Router  } from '@angular/router';
 @Injectable()
 export class ServiceService {
 
-  url = "http://localhost:3000/";
+  url = "";
   private userName;
   private email;
   option;
@@ -26,42 +26,42 @@ export class ServiceService {
   profile()
   {
     this.createHeader();
-    return this.http.get('http://localhost:3000/user/profile',this.option).map((res=>res.json()));
+    return this.http.get(this.url+'user/profile',this.option).map((res=>res.json()));
   }
 
 
   changeUserName(userName)
   {
     this.createHeader();
-    return this.http.post('http://localhost:3000/user/changeUserName',userName , this.option).map((res)=>res.json());
+    return this.http.post(this.url+'user/changeUserName',userName , this.option).map((res)=>res.json());
   }
 
 
   finalChangeUserName(userName)
   {    
     this.createHeader();
-    return this.http.post('http://localhost:3000/user/finalChangeUserName',userName,this.option)
+    return this.http.post(this.url+'user/finalChangeUserName',userName,this.option)
     .map((res)=>res.json());
   }
 
 
   checkEmail(email)
   {
-    return this.http.post('http://localhost:3000/user/checkEmail',email , this.option).map((res)=>res.json());
+    return this.http.post(this.url+'user/checkEmail',email , this.option).map((res)=>res.json());
   }
 
 
   finalChangeEmail(Email)
   {
     this.createHeader();
-    return this.http.post('http://localhost:3000/user/finalChangeEmail',Email,this.option)
+    return this.http.post(this.url+'user/finalChangeEmail',Email,this.option)
     .map((res)=>res.json());
   }
 
   Change_Password(Password)
   {
     this.createHeader();
-    return this.http.post('http://localhost:3000/user/changePassword',Password,this.option)
+    return this.http.post(this.url+'user/changePassword',Password,this.option)
     .map((res)=>res.json());
   }
 
